86490c4f17607a56baa38b1e0692d5b1bc450c84
Nguyen Duc Tuan <71483286+tuanndph16237@users.noreply.github.com>
2024-05-06 11:57:21 +0700

    auth and layout

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,18 +2,24 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import { v2 as cloudinary } from "cloudinary";
+import myUserRoute from "./routes/MyUserRoute";
 
 mongoose
 .connect(process.env.MONGODB_CONNECTION_STRING as string)
 .then(() => console.log("Connected to database!"));
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) =>{
-    res.json({ message:"Hello!" });
-});
+app.use("/api/my/user", myUserRoute);
 app.listen(6000, () =>{
     console.log("server started in localhost:6000");
 });
--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,15 +1,54 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-
-function AppRoutes() {
-
-
+import Layout from "./layouts/layout";
+import HomePage from "./pages/HomePage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
+import SearchPage from "./pages/SearchPage";
+import OrderStatusPage from "./pages/OrderStatusPage";
+const AppRoutes = () => {
   return (
     <Routes>
-      <Route />
-      <Route />
-      <Route />
-    </Routes>
-  )
-}
+      <Route
+        path="/"
+        element={
+          <Layout showHero>
+            <HomePage />
+          </Layout>
+        }
+      />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      <Route
+        path="/search/:city"
+        element={
+          <Layout showHero={false}>
+            <SearchPage />
+          </Layout>
+        }
+      />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/order-status"
+          element={
+            <Layout>
+              <OrderStatusPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+        
+      </Route>
 
-export default AppRoutes
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
